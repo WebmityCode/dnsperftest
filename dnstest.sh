4d0fcf5db0a8cc3f8f4a3e1a6c29388f3d829f61
Webminty <git@webminty.com>
2024-09-13 08:00:38 -0700

    Some clean up (@thomasmerzj)

--- dnstest.sh
+++ dnstest.sh
@@ -5,20 +5,20 @@ command -v bc > /dev/null || { echo "error: bc was not found. Please install bc.
 { command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "error: dig was not found. Please install dnsutils."; exit 1; }
 
 
-NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
+NAMESERVERS=$(grep ^nameserver /etc/resolv.conf | cut -d " " -f 2 | sed 's/\(.*\)/&#&/')
 
 PROVIDERSV4="
-1.1.1.1#cloudflare 
-4.2.2.1#level3 
-8.8.8.8#google 
-9.9.9.9#quad9 
-80.80.80.80#freenom 
+1.1.1.1#cloudflare
+4.2.2.1#level3
+8.8.8.8#google
+9.9.9.9#quad9
+80.80.80.80#freenom
 208.67.222.123#opendns 
-199.85.126.20#norton 
-185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
+199.85.126.20#norton
+185.228.168.168#cleanbrowsing
+77.88.8.7#yandex
+176.103.130.132#adguard
+156.154.70.3#neustar
 8.26.56.26#comodo
 45.90.28.202#nextdns
 "
@@ -63,8 +63,7 @@ fi
 
     
 
-# Domains to test. Duplicated domains are ok
-#DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+# Domains to test. Add or change them in the included top-domains.txt
 DOMAINS2TEST=$(head top-domains.txt)
 
 totaldomains=0
@@ -84,7 +83,7 @@ for p in $NAMESERVERS $providerstotest; do
 
     printf "%-21s" "$pname"
     for d in $DOMAINS2TEST; do
-        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+        ttime=$($dig +tries=1 +time=2 +stats "@$pip" "$d" |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2)
         if [ -z "$ttime" ]; then
 	        #let's have time out be 1s = 1000ms
 	        ttime=1000
@@ -95,7 +94,7 @@ for p in $NAMESERVERS $providerstotest; do
         printf "%-8s" "$ttime ms"
         ftime=$((ftime + ttime))
     done
-    avg=`bc -l <<< "scale=2; $ftime/$totaldomains"`
+    avg=$(bc -lq <<< "scale=2; $ftime/$totaldomains")
 
     echo "  $avg"
 done
